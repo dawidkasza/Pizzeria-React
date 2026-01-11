a39b139118e29bb144f34bad0fae75704d434e86
Dawid Kasza <dawidkasza05@gmail.com>
2026-01-11 12:36:28 +0100

    Build app

--- src/config.js
+++ src/config.js
@@ -1,3 +1,1 @@
-export const API_URL = process.env.NODE_ENV === 'production'
-  ? `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co/api`
-  : 'http://localhost:3131/api';
+export const API_URL = process.env.NODE_ENV === 'production' ?  '/api' : 'http://localhost:3131/api'
